58ee9733a769a220f1dbb3f0a9f22caddc9b742d
shency <alex_shency@outlook.com>
2024-10-16 20:25:19 +0800

    kafka demo

---
+++ kafka/src/main/java/top/codingshen/Main.java
@@ -0,0 +1,13 @@
+package top.codingshen;
+
+/**
+ * @ClassName ${NAME}
+ * @Description description
+ * @Author alex_shen
+ * @Date 2024/9/29 - 22:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
